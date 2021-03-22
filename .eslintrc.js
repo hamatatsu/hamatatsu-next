bf90fc71386ef5098cf9e079dfe4e1b86f3b9075
hamatatsu <tatsuyahamada.g@gmail.com>
2021-03-22 18:44:05 +0900

    Add config files

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  'env': {
+    'es2021': true,
+    'node': true,
+  },
+  'extends': [
+    'plugin:react/recommended',
+    'google',
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true,
+    },
+    'ecmaVersion': 12,
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'react',
+    '@typescript-eslint',
+  ],
+  'rules': {
+    'max-len': 'off',
+    'require-jsdoc': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+};
