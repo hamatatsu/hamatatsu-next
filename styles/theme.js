cd2da6fed78292062cdd8c403d22b42f5b66c8fa
hamatatsu <tatsuyahamada.g@gmail.com>
2021-03-24 01:34:27 +0900

    Change color

--- styles/theme.js
+++ styles/theme.js
@@ -1,5 +1,14 @@
 import {createMuiTheme} from '@material-ui/core/styles';
 
-const theme = createMuiTheme({});
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#4fc3f7',
+    },
+    secondary: {
+      main: '#ff9800',
+    },
+  },
+});
 
 export default theme;
