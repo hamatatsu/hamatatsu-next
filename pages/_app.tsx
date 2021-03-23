1b78c5e518ca3c3ac3fd192b1fc27677a2153186
hamatatsu <tatsuyahamada.g@gmail.com>
2021-03-23 23:10:02 +0900

    Import Material-UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css';
 import type {AppProps} from 'next/app';
 import Head from 'next/head';
+import theme from '../styles/theme';
+import React from 'react';
 
 export default function MyApp({Component, pageProps}: AppProps) {
+  React.useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
   return (
     <>
       <Head>
@@ -11,6 +19,7 @@ export default function MyApp({Component, pageProps}: AppProps) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="theme-color" content={theme.palette.primary.main} />
       </Head>
       <Component {...pageProps} />
     </>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,22 @@
+import React from 'react';
 import Document, {DocumentContext, Html, Head, Main, NextScript} from 'next/document';
+import {ServerStyleSheets} from '@material-ui/core/styles';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
+    const sheets = new ServerStyleSheets();
+    const originalRenderPage = ctx.renderPage;
+
+    ctx.renderPage =() =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+      });
+
     const initialProps = await Document.getInitialProps(ctx);
-    return {...initialProps};
+    return {
+      ...initialProps,
+      styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
+    };
   }
 
   render() {
