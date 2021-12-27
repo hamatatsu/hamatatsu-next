bfbb9c00381f58131366c4f3cae4cc39014964b8
hamatatsu <6399551+hamatatsu@users.noreply.github.com>
2021-12-27 11:58:28 +0000

    Fix format

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -26,7 +26,8 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700&display=swap" rel="stylesheet" />
-        </Head>        <body>
+        </Head>
+        <body>
           <Main />
           <NextScript />
         </body>
