8b2e381844357292a12a3b4e80767f9aeae23a1e
hamatatsu <6399551+hamatatsu@users.noreply.github.com>
2024-02-11 20:25:12 +0900

    Update social button image import

--- src/app/_components/social-btn.tsx
+++ src/app/_components/social-btn.tsx
@@ -1,14 +1,14 @@
 import styles from "@/app/_components/social-btn.module.css";
 import Image from "next/image";
 import github from "/public/github.svg";
-import x from "/public/x.svg";
+import x_twitter from "/public/x-twitter.svg";
 
 export default function SocialButton() {
   return (
     <div className={styles.root}>
       <div className={styles.socialButton}>
         <a className={"socialButton"} href="https://twitter.com/hamatatsu0">
-          <Image src={x} alt="x" fill style={{ background: "black" }} />
+          <Image src={x_twitter} alt="x" fill style={{ background: "black" }} />
         </a>
       </div>
       <div className={styles.socialButton}>
